7d9bd66894a0c1815d7221849d3a191e1569838c
Marcus Jiles <jilesm@mail.gvsu.edu>
2019-08-05 14:12:51 -0400

    Updated scroll and css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import (
+import(
     "https://cferdinandi.github.io/smooth-scroll/dist/smooth-scroll.js"
-).then(function(){
-    new SmoothScroll('a[href*="#"]'); 
-    console.log("Worked");
-});
+  ).then(function() {
+    new SmoothScroll('a[href*="#"]'); // eslint-disable-line
+    console.log("resolved!");
+  });
